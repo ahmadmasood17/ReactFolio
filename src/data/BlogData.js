e3f7ffef046b167071cfa36759937ea06b539b7c
Ahmad <ahmadrajpoot5656565656@gmail.com>
2025-08-01 23:44:49 +0500

    First commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,37 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Webdevelopment Roadmap For Freshers",
+        tags:["webdevelopment",],
+        date:"13 May, 2021",
+        imgSrc:"https://cdn.prod.website-files.com/6344c9cef89d6f2270a38908/673f2a3b44c1ed4901bb43bb_6386328bea96dffacc89946b_d1.webp",
+        link:"https://laraabouts.blogspot.com/2024/12/web-development-roadmap%20.html"
+    },
+    {
+        id:2,
+        name:"What is Html",
+        tags:["HTML"],
+        date:"5 July, 2021",
+        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj5K08rKxUEHZsgxTHElnQc6bFEmuVzD6FUg&s",
+        link:"https://laraabouts.blogspot.com/2024/12/what-is-html.html"
+    },
+    {
+        id:1,
+        name:"What is PHP ",
+        tags:["php",],
+        date:"13 May, 2021",
+        imgSrc:"https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+        link:"https://laraabouts.blogspot.com/2024/11/what-is-php.html"
+    },
+    {
+        id:2,
+        name:"What is Laravel",
+        tags:["php", "laravel"],
+        date:"5 July, 2021",
+        imgSrc:"https://www.regur.net/_next/image/?url=https%3A%2F%2Fi0.wp.com%2Fcms.regur.net%2Fwp-content%2Fuploads%2F2025%2F02%2Fwhat-is-laravel.webp%3Ffit%3D1200%252C720%26ssl%3D1&w=2048&q=75",
+        link:"https://laraabouts.blogspot.com/2024/11/what-is-laravel.html"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,52 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Nuzzler Landing Page",
+        description:"It is build on top of the Html CSS JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["Js","gsap","styledComponents"],
+        
+        demo:"https://nuzzler-6ym9.vercel.app/",
+        github:"https://github.com/ahmadmasood17/Nuzzler"
+    },
+    {
+        id:2,
+        name:"ESA Helper website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","sass", "tailwind"],
+        
+        demo:"https://phpstack-1250693-5680655.cloudwaysapps.com/",
+        github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Dev.to Clone",
+        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["react","api","sass"],
+        
+        demo:" ",
+        github:" "
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"  ",
+        github:" "
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:" ",
+        github:" "
+        
+    },
+    
+] 
